e7fb257b397c9b717092d42bf9360fac51e11a30
Panasun Sunanta <panasunsunanta@gmail.com>
2023-11-04 10:09:48 +0700

    fix

--- lib/gurufocus_scrape.ex
+++ lib/gurufocus_scrape.ex
@@ -39,7 +39,87 @@ defmodule GuruFocusScrape do
     DB.put({:stock, ticker, :summary_html}, result)
   end
 
+  def get_stock_summary_html(ticker) do
+    DB.get({:stock, ticker, :summary_html})
+  end
+
   def get_stock(ticker) do
-    result = DB.get({:stock, ticker, :summary_html}) |> IO.inspect()
+    DB.get({:stock, ticker, :summary})
+  end
+
+  def clean_title(title) do
+    title
+    |> String.replace("\n", "")
+    |> String.replace("-", "_")
+    |> String.replace("(", " ")
+    |> String.replace(")", "")
+    |> String.replace("%", "")
+    |> String.replace("$", "")
+    |> String.replace("  ", " ")
+    |> String.trim()
+    |> String.replace(" ", "_")
+    |> String.downcase()
+  end
+
+  def parse_summary_data(ticker) do
+    html = get_stock_summary_html(ticker)
+    {:ok, document} = Floki.parse_document(html)
+
+    data1 =
+      document
+      |> Floki.find(".stock-indicators-table-row")
+      |> Enum.map(fn tr_element ->
+        td_elements = Floki.find(tr_element, "td")
+
+        key =
+          td_elements
+          |> Enum.at(1)
+          |> Floki.text()
+          |> clean_title()
+
+        value =
+          td_elements
+          |> Enum.at(2)
+          |> Floki.text()
+          |> String.trim()
+
+        {key, value}
+      end)
+
+    data2 =
+      document
+      |> Floki.find(".t-caption")
+      |> Enum.map(fn tr_element ->
+        td_elements = Floki.find(tr_element, "td")
+
+        key =
+          td_elements
+          |> Enum.at(1)
+          |> Floki.text()
+          |> clean_title()
+
+        value =
+          td_elements
+          |> Enum.at(2)
+          |> Floki.text()
+          |> String.trim()
+
+        {key, value}
+      end)
+      |> Enum.filter(fn {key, value} ->
+        key != nil and key != ""
+      end)
+
+    data =
+      (data1 ++ data2)
+      |> Enum.reduce(%{}, fn {key, value}, acc ->
+        Map.update(acc, key, value, fn current_value ->
+          current_value
+        end)
+      end)
+
+    DB.put({:stock, ticker, :summary}, data)
+
+    data
   end
 end
